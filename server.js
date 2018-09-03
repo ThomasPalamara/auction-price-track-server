2f83bb97b6560a20dd593355afa50e4a33f574bd
ThomasPalamara <toma06pal@gmail.com>
2018-09-03 21:49:50 +0100

    Adding CORS

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const cors = require('cors');
 const routerRealms = require('./router/router_realms');
 const routerAuctions = require('./router/router_auctions');
 const routerDB = require('./router/router_initdb');
@@ -9,6 +10,8 @@ const config = require('./config');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(morgan(config.morganFormat));
 
--- winston.js
+++ winston.js
@@ -9,6 +9,11 @@ const logger = winston.createLogger({
     level: 'debug',
     transports: [
         new winston.transports.Console(),
+        new winston.transports.File({ 
+            filename: 'server.log',
+            maxsize: 5242880, // 5MB
+            maxFiles: 10,
+        })
     ],
     format: winston.format.combine(
         winston.format.timestamp(),
