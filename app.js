2977f6c836a3ec107b32ecb5ed03bb43b23edd0f
Ludovic <ludovic.sinquin@gmail.com>
2018-12-15 06:18:18 +0100

    Mise en place de la documentation de l'API au endpoint /api-docs

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const winston = require('./config/winston');
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const initDbConnexion = require('./config/db');
 const initRoutes = require('./routes');
 
 const app = express();
+
 initDbConnexion();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 initRoutes(app);
 
 const port = process.env.PORT || 5000;
