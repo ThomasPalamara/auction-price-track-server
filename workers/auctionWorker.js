d2bb2e2709c5250a6093a6c3a17ba54690162363
Ludovic <ludovic.sinquin@gmail.com>
2019-01-10 04:08:46 +0100

    Ajout de logs au niveau des crons

--- workers/auctionWorker.js
+++ workers/auctionWorker.js
@@ -1,6 +1,10 @@
 const CronJob = require('cron').CronJob;
 const auctionService = require('../services/auction.srv');
 
+console.log("Setting Auctions' Workers");
+
 const auctionJob = new CronJob('0 */2 * * *', auctionService.refreshAuctionsData, () => console.log('cron job correctly ran'));
 
 auctionJob.start();
+
+console.log(`Next Cron starting at ${auctionJob.nextDates()}`);
