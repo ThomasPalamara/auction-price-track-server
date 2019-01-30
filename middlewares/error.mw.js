8a6a1d5c96cf189297add2bebf17e0fa4f2513ee
Ludovic <ludovic.sinquin@gmail.com>
2019-01-30 03:38:43 +0100

    Fix de la gestion d'erreurs centralisée

--- middlewares/error.mw.js
+++ middlewares/error.mw.js
@@ -1,6 +1,7 @@
 const winston = require('../config/winston');
 
-module.exports = (error, req, res) => {
+// eslint-disable-next-line no-unused-vars
+module.exports = (error, req, res, next) => {
     winston.error(error.message);
     res.status(500).send({ message: 'Unfortunate things happened' });
 };
